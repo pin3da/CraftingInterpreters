eabdcead810f33bc325a549206788089577230f0
Manuel Pineda <manuelpineda@google.com>
2021-01-04 16:42:30 +0100

    Add basic expressions and AstPrinter.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # CraftingInterpreters
 
 Kotlin implementation of https://craftinginterpreters.com
+
+
+## Notes
+
+- Unlike the original code, this one does not use the visitor pattern. Instead,
+this code implements a "pattern matching"-like approach using when statements
+and sealed classes. As result, the code makes a linear search across all the
+subtypes of Expr (instead of the constant indirection in the visitor pattern),
+but removes a lot of boilerplate and code generation.
---
+++ src/AstPrinter.kt
@@ -0,0 +1,27 @@
+class AstPrinter {
+  var buffer = StringBuilder()
+
+  fun print(expr: Expr): String {
+    buffer = StringBuilder()
+    traverse(expr)
+    return buffer.toString()
+  }
+
+  private fun traverse(expr: Expr) {
+    when (expr) {
+      is Literal -> buffer.append(expr.value.toString())
+      is Binary -> parenthesize(expr.op.lexeme, expr.left, expr.right)
+      is Unary -> parenthesize(expr.op.lexeme, expr.expr)
+      is Grouping -> parenthesize("group", expr.expr)
+    }
+  }
+
+  private fun parenthesize(name: String, vararg exprs: Expr) {
+    buffer.append("(").append(name)
+    for (expr in exprs) {
+      buffer.append(" ")
+      traverse(expr)
+    }
+    buffer.append(")")
+  }
+}
