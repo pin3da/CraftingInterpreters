f7f1237ad225d482aa2f6f78de1b62e2fafc6ebf
Manuel Pineda <manuelpineda@google.com>
2021-01-05 21:18:53 +0100

    [WIP] Add Parser:
    Missing synchronize method, add tests, and connect with the Lox class.
    
    Also added .editorconfig to avoid issues between IntellIJ and Ktlint.

--- src/main/kotlin/ErrorReporter.kt
+++ src/main/kotlin/ErrorReporter.kt
@@ -2,6 +2,9 @@ interface ErrorReporterInterface {
     var hadError: Boolean
 
     fun error(line: Int, message: String)
+    fun error(token: Token, message: String) {
+        error("override if needed")
+    }
 }
 
 class ErrorReporter : ErrorReporterInterface {
@@ -9,6 +12,14 @@ class ErrorReporter : ErrorReporterInterface {
 
     override fun error(line: Int, message: String) {
         report(line, "", message)
+    }
+
+    override fun error(token: Token, message: String) {
+        val where = when (token.type) {
+            TokenType.EOF -> "at end"
+            else -> "at '${token.lexeme}'"
+        }
+        report(token.line, where, message)
     }
 
     private fun report(line: Int, where: String, message: String) {
---
+++ src/main/kotlin/Parser.kt
@@ -0,0 +1,136 @@
+class Parser(
+    private val tokens: ArrayDeque<Token>,
+    private val errorReporter: ErrorReporterInterface
+) {
+    private class ParseError : RuntimeException()
+
+    private var current = 0
+
+    private fun expression(): Expr {
+        return equality()
+    }
+
+    private fun equality(): Expr {
+        var expr = comparison()
+
+        while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+            val operator = previous()
+            val right = comparison()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun previous(): Token {
+        return tokens[current - 1]
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        for (type in types) {
+            if (check(type)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun advance(): Token {
+        if (!isAtEnd()) {
+            current++
+        }
+        return previous()
+    }
+
+    private fun comparison(): Expr {
+        var expr = term()
+
+        while (match(
+                TokenType.GREATER,
+                TokenType.GREATER_EQUAL,
+                TokenType.LESS,
+                TokenType.LESS_EQUAL
+            )
+        ) {
+            val operator = previous()
+            val right = term()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun term(): Expr {
+        var expr = factor()
+
+        while (match(TokenType.MINUS, TokenType.PLUS)) {
+            val operator = previous()
+            val right = factor()
+            expr = Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    private fun factor(): Expr {
+        var expr = unary()
+
+        while (match(TokenType.SLASH, TokenType.STAR)) {
+            val operator = previous()
+            val right: Expr = unary()
+            expr = Binary(expr, operator, right)
+        }
+
+        return expr
+    }
+
+    private fun unary(): Expr {
+        if (match(TokenType.BANG, TokenType.MINUS)) {
+            val operator = previous()
+            val right = unary()
+            return Unary(operator, right)
+        }
+
+        return primary()
+    }
+
+    private fun primary(): Expr {
+        return when {
+            match(TokenType.FALSE) -> Literal(false)
+            match(TokenType.TRUE) -> Literal(true)
+            match(TokenType.NIL) -> Literal(null)
+            match(TokenType.NUMBER, TokenType.STRING) -> Literal(previous().literal)
+            match(TokenType.LEFT_PAREN) -> {
+                val expr = expression()
+                consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+                Grouping(expr)
+            }
+            else -> error("invalid state when parsing primary expression")
+        }
+    }
+
+    private fun consume(type: TokenType, message: String): Token {
+        if (check(type)) {
+            return advance()
+        }
+        throw error(peek(), message)
+    }
+
+    private fun error(token: Token, message: String): ParseError {
+        errorReporter.error(token, message)
+        return ParseError()
+    }
+
+    private fun check(type: TokenType): Boolean {
+        if (isAtEnd()) {
+            return false
+        }
+        return peek().type === type
+    }
+
+    private fun peek(): Token {
+        return tokens[current]
+    }
+
+    private fun isAtEnd(): Boolean {
+        return peek().type == TokenType.EOF
+    }
+}
